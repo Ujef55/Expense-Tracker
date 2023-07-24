5a8daabcee69e9b8160bbf5b4d3993a58f6489bb
Ujef55 <ujef.kelawala123@gmail.com>
2023-07-24 18:43:06 +0530

    Created new custom component ExpenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+    return (
+      <div className="expense-item">
+        <div>April 25th 2022</div>
+        <div className="expense-item-description">
+            <h2>Bought a new phone</h2>
+            <div className="expense-item-price">$422.89</div>
+        </div>
+      </div>
+    );
+  }
