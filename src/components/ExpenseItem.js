fc37b061878833b0f15b9f7da7b99b8df7db9f6f
Ujef55 <ujef.kelawala123@gmail.com>
2023-07-25 21:04:02 +0530

    passing data via props - learning props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-    const ExpenseDate = new Date(2022, 3, 25);
-    const expenseTitle = 'Bought a new phone';
-    const expenseAmount = 422.89;
+    // const ExpenseDate = new Date(2022, 3, 25);
+    // const expenseTitle = 'Bought a new phone';
+    // const expenseAmount = 422.89;
 
     return (
       <div className="expense-item">
-        <div>{ExpenseDate.toLocaleDateString()}</div>
+        <div>{props.date.toDateString()}</div>
         <div className='expense-item-description'>
-          <h2>{expenseTitle}</h2>
-          <div className='expense-item-price'>{expenseAmount}</div>
+          <h2>{props.title}</h2>
+          <div className='expense-item-price'>{props.amount}</div>
         </div>
       </div>
     );
