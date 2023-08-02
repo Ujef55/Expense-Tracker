1edca06a79d41864a11516db11401aeb6795c954
Ujef55 <ujef.kelawala123@gmail.com>
2023-08-03 03:05:29 +0530

    NewExpenses component created

---
+++ src/components/NewExpenses/NewExpenses.js
@@ -0,0 +1,12 @@
+import NewExpensesForm from './NewExpensesForm';
+import './NewExpensesForm.css';
+
+const NewExpenses = () => {
+    return (
+        <div className='new-expense'>
+            <NewExpensesForm />
+        </div>
+    )
+}
+
+export default NewExpenses;
