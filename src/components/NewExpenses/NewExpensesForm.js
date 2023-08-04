1cb26d361e31dea6566f0b5cc7700c0b59366af2
Ujef55 <ujef.kelawala123@gmail.com>
2023-08-04 16:52:24 +0530

    clearing input value using two way binding

--- src/components/NewExpenses/NewExpensesForm.js
+++ src/components/NewExpenses/NewExpensesForm.js
@@ -29,6 +29,10 @@ function NewExpensesForm() {
         }
 
         console.log(expenseData);
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     }
 
 
@@ -37,15 +41,18 @@ function NewExpensesForm() {
             <div className='new-expense-controls'>
                 <div className='new-expense-control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler}/>
+                    <input type='text' value={enteredTitle} 
+                    onChange={titleChangeHandler}/>
                 </div> 
                 <div className='new-expense-control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+                    <input type='number' min='0.01' step='0.01' value={enteredAmount}
+                     onChange={amountChangeHandler} />
                 </div>   
                 <div className='new-expense-control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangeHandler}/>
+                    <input type='date' min='2019-01-01' max='2022-12-31' value={enteredDate}
+                     onChange={dateChangeHandler}/>
                 </div>   
             </div>
             <div className='new-expense-actions'>
